3a687d734c01219aa55d98b4f5d08e809e24d87e
Chaitanyaputhran <21i07.chaitanya@sjec.ac.in>
2023-05-26 22:31:41 +0530

    updated index.html

---
+++ index.js
@@ -0,0 +1,8 @@
+const john =document.querySelectorAll(".john");
+const tanya =document.querySelectorAll(".tanya");
+const prev = document.querySelectorAll(".prev__button");
+const next = document.querySelectorAll(".next__button");
+
+john.classList.add("hide");
+
+prev.forEach(item => item.addEventListener(""))
